bbb7cf658d0cb642b8997ef7f4d877fa91d7cf65
Victor Lopes <125214784+VictorLPSRD@users.noreply.github.com>
2023-06-23 02:03:42 -0300

    Update jogodepalavras.py

--- jogodepalavras.py
+++ jogodepalavras.py
@@ -1,37 +1,37 @@
-palavra=input('Digite uma palavra secreta :')
+palavra=input('Digite uma palavra secreta :') # Você vai digitar a palavra.
 
-import os
+import os # importei o módulo os .
 
-os.system('cls')
+os.system('cls') # usei(cls) para não fica exibindo a palavra que você colocou ele vai limpa o terminal. 
 
-palavra_secreta = palavra
-letras_acertadas = ''
-numero_tentativas = 0
+palavra_secreta = palavra # a palavra fica salva nessa variavel.
+letras_acertadas = '' # Para salva as letras acertadas e exibir.
+numero_tentativas = 0 # numero de acertos começa com zero.
 
-while True:
-    letra_digitada = input('Digite uma letra: ')
-    numero_tentativas += 1
+while True: # Crie um laço de repetição.
+    letra_digitada = input('Digite uma letra: ') # pega a letra que o usuario digitou.
+    numero_tentativas += 1 # A cada letra digitada vai adicionar um a numero_tentativas.
 
-    if len(letra_digitada) > 1:
-        print('Digite apenas uma letra.')
-        continue
+    if len(letra_digitada) > 1: # Se o usuario digita mais de uma letra .
+        print('Digite apenas uma letra.') # Sera exibido isso.
+        continue # ele volta ao começo do laço .
 
-    if letra_digitada in palavra_secreta:
-        letras_acertadas += letra_digitada
+    if letra_digitada in palavra_secreta: # Se a letra_digitada estiver em palavra_digitada.
+        letras_acertadas += letra_digitada # A cada letra_acertada ele adiciona a letra em letras_acertadas.
 
-    palavra_formada = ''
-    for letra_secreta in palavra_secreta:
-        if letra_secreta in letras_acertadas:
-            palavra_formada += letra_secreta
-        else:
-            palavra_formada += '*'
+    palavra_formada = ''# Essa variavel serve para a palra_formada fica na horizontal e exibir ela de uma vez. 
+    for letra_secreta in palavra_secreta: # Crie um for para pecore letra por letra.
+        if letra_secreta in letras_acertadas: # Se a letr_secreta estiver em letras_acertadas.
+            palavra_formada += letra_secreta  # Ele vai adicionar a letra em letras_secreta e exibir ela no lugar do asterisco(*).
+        else: # Se não 
+            palavra_formada += '*' # ele vai exibir o asterisco(*).
 
-    print('Palavra formada:', palavra_formada)
+    print('Palavra formada:', palavra_formada) # fora do for ele vai exibir ou a letra ou asterisco(*).
 
-    if palavra_formada == palavra_secreta:
-        os.system('clear')
-        print('VOCÊ GANHOU!! PARABÉNS!')
-        print('A palavra era', palavra_secreta)
-        print('Tentativas:', numero_tentativas)
-        letras_acertadas = ''
-        numero_tentativas = 0
+    if palavra_formada == palavra_secreta: # Se palavra_formada for igual a palavra_secreta 
+        os.system('clear')# Ele vai limpa o terminal.
+        print('VOCÊ GANHOU!! PARABÉNS!') # Vai exibir isso.
+        print('A palavra era', palavra_secreta) # Mostra a palrava_secreta .
+        print('Tentativas:', numero_tentativas) # Vai mostra o numero de tentivas. 
+        letras_acertadas = '' # vai zera as letras_acertadas.
+        numero_tentativas = 0 # e zera numero de tentativas.
